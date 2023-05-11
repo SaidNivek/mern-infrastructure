b527dc1d708745ad39d796e0ce603e33fe55b0e7
SaidNivek <kevin.vi.dias@gmail.com>
2023-05-10 21:32:12 -0400

    part 2 complete

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,29 @@
+import './App.css';
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import NewOrderPage from '../NewOrder/NewOrderPage';
+import AuthPage from '../Auth/AuthPage';
+import OrderHistoryPage from '../OrderHistory/OrderHistoryPage';
+import NavBar from '../../components/NavBar';
+
+function App() {
+  const [user, setUser] = useState({})
+  
+  return (
+    <main className="App">
+      { user ?
+        <>
+        <NavBar />
+          <Routes>
+            <Route path="/orders/new" element={<NewOrderPage />}></Route>
+            <Route path="/orders" element={<OrderHistoryPage />}></Route>
+          </Routes>
+        </>
+        :
+        <AuthPage />
+      }
+    </main>
+  );
+}
+
+export default App;
---
+++ src/pages/OrderHistory/OrderHistoryPage.jsx
@@ -0,0 +1,8 @@
+
+function OrderHistoryPage() {
+  return (
+    <h1>OrderHistoryPage</h1>
+  )
+}
+
+export default OrderHistoryPage
